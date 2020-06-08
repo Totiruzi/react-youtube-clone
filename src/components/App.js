e6f273e1857ce65edbc6b1d42fb72302fa1430e5
Onowu Christopher <totiruzi@yahoo.com>
2020-06-08 03:22:42 +0300

    Create detail component and displayed video list

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,37 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
+const KEY = 'AIzaSyA5hvJ7N4rNSNnUaOO_S-HNKGYct6UOn4Q';
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        part: 'snippet',
+        type: 'video',
+        maxResult: 5,
+        key: KEY,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
   render() {
     return (
-      <div>
-        <SearchBar />
+      <div className="ui container">
+        <SearchBar onTermSubmite={this.onTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
